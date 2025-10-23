67c34379be55a217a25265e3ee59edb28faf55c0
Ben Gawel <ben.gawel@mongodb.com>
2025-10-23 20:09:47 +0000

    use string instead of char array and vector instead of pointer to array

--- api/include/opentelemetry/common/kv_properties.h
+++ api/include/opentelemetry/common/kv_properties.h
@@ -140,57 +140,34 @@ public:
   class Entry
   {
   public:
-    Entry() : key_(nullptr), value_(nullptr) {}
+    Entry() = default;
 
     // Copy constructor
-    Entry(const Entry &copy)
-    {
-      key_   = CopyStringToPointer(copy.key_.get());
-      value_ = CopyStringToPointer(copy.value_.get());
-    }
+    Entry(const Entry &copy) = default;
 
     // Copy assignment operator
-    Entry &operator=(Entry &other)
-    {
-      key_   = CopyStringToPointer(other.key_.get());
-      value_ = CopyStringToPointer(other.value_.get());
-      return *this;
-    }
+    Entry &operator=(const Entry &other) = default;
 
     // Move contructor and assignment operator
-    Entry(Entry &&other)            = default;
+    Entry(Entry &&other) = default;
     Entry &operator=(Entry &&other) = default;
 
     // Creates an Entry for a given key-value pair.
     Entry(nostd::string_view key, nostd::string_view value)
-    {
-      key_   = CopyStringToPointer(key);
-      value_ = CopyStringToPointer(value);
-    }
+      : key_(key), value_(value) {}
 
     // Gets the key associated with this entry.
-    nostd::string_view GetKey() const noexcept { return key_.get(); }
+    nostd::string_view GetKey() const noexcept { return key_; }
 
     // Gets the value associated with this entry.
-    nostd::string_view GetValue() const noexcept { return value_.get(); }
+    nostd::string_view GetValue() const noexcept { return value_; }
 
     // Sets the value for this entry. This overrides the previous value.
-    void SetValue(nostd::string_view value) noexcept { value_ = CopyStringToPointer(value); }
+    void SetValue(nostd::string_view value) noexcept { value_ = value; }
 
   private:
-    // Store key and value as raw char pointers to avoid using std::string.
-    nostd::unique_ptr<const char[]> key_;
-    nostd::unique_ptr<const char[]> value_;
-
-    // Copies string into a buffer and returns a unique_ptr to the buffer.
-    // This is a workaround for the fact that memcpy doesn't accept a const destination.
-    nostd::unique_ptr<const char[]> CopyStringToPointer(nostd::string_view str)
-    {
-      char *temp = new char[str.size() + 1];
-      memcpy(temp, str.data(), str.size());
-      temp[str.size()] = '\0';
-      return nostd::unique_ptr<const char[]>(temp);
-    }
+    nostd::string_view key_;
+    nostd::string_view value_;
   };
 
   // Maintain the number of entries in entries_.
@@ -199,29 +176,28 @@ public:
   // Max size of allocated array
   size_t max_num_entries_;
 
-  // Store entries in a C-style array to avoid using std::array or std::vector.
-  nostd::unique_ptr<Entry[]> entries_;
+  std::vector<Entry> entries_;
 
 public:
   // Create Key-value list of given size
   // @param size : Size of list.
   KeyValueProperties(size_t size) noexcept
-      : num_entries_(0), max_num_entries_(size), entries_(new Entry[size])
+      : num_entries_(0), max_num_entries_(size), entries_(size)
   {}
 
   // Create Empty Key-Value list
-  KeyValueProperties() noexcept : num_entries_(0), max_num_entries_(0), entries_(nullptr) {}
+  KeyValueProperties() noexcept : num_entries_(0), max_num_entries_(0) {}
 
   template <class T, class = typename std::enable_if<detail::is_key_value_iterable<T>::value>::type>
   KeyValueProperties(const T &keys_and_values) noexcept
       : num_entries_(0),
         max_num_entries_(keys_and_values.size()),
-        entries_(new Entry[max_num_entries_])
+        entries_(max_num_entries_)
   {
     for (auto &e : keys_and_values)
     {
       Entry entry(e.first, e.second);
-      (entries_.get())[num_entries_++] = std::move(entry);
+      entries_[num_entries_++] = std::move(entry);
     }
   }
 
@@ -231,7 +207,7 @@ public:
     if (num_entries_ < max_num_entries_)
     {
       Entry entry(key, value);
-      (entries_.get())[num_entries_++] = std::move(entry);
+      entries_[num_entries_++] = std::move(entry);
     }
   }
 
@@ -241,7 +217,7 @@ public:
   {
     for (size_t i = 0; i < num_entries_; i++)
     {
-      auto &entry = (entries_.get())[i];
+      auto &entry = entries_[i];
       if (!callback(entry.GetKey(), entry.GetValue()))
       {
         return false;
@@ -255,7 +231,7 @@ public:
   {
     for (size_t i = 0; i < num_entries_; i++)
     {
-      auto &entry = (entries_.get())[i];
+      auto &entry = entries_[i];
       if (entry.GetKey() == key)
       {
         const auto &entry_value = entry.GetValue();
